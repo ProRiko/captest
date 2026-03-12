a15bf1e1d24bd3250c72b8e90e7190cd24b3abe3
ProRiko <reebhu26@gmai.com>
2026-03-12 10:16:20 +0530

    First Commit

--- src/jenkins/com/capgemini/Demo.java
+++ src/jenkins/com/capgemini/Demo.java
@@ -3,7 +3,9 @@ package jenkins.com.capgemini;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
 
 	}
 
